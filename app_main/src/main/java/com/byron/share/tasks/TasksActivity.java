0619fca6455d91bba1ff3a48f17c737c534088b7
machuang <machuang0104@126.com>
2016-07-29 01:07:00 +0800

    change main activity

--- app_main/src/main/java/com/byron/share/tasks/TasksActivity.java
+++ app_main/src/main/java/com/byron/share/tasks/TasksActivity.java
@@ -29,7 +29,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.byron.share.Injection;
-import com.example.android.architecture.blueprints.todoapp.R;
+import com.byron.share.R;
 import com.byron.share.statistics.StatisticsActivity;
 import com.byron.share.util.ActivityUtils;
 import com.byron.share.util.EspressoIdlingResource;
